fe0c1ad0eb9ba137c1d45aab12f6585887107656
Radphuchit <radphuchit@gmail.com>
2022-09-08 21:53:22 +0700

    refact code

--- simple_hand_detect.py
+++ simple_hand_detect.py
@@ -1,75 +1,78 @@
 import cv2
+import time
 import mediapipe as mp
 
-def get_bbox_coordinates(handLandmark, image_shape):
-    """ 
-    Get bounding box coordinates for a hand landmark.
-    Args:
-        handLadmark: A HandLandmark object.
-        image_shape: A tuple of the form (height, width).
-    Returns:
-        A tuple of the form (xmin, ymin, xmax, ymax).
-    """
-    all_x, all_y = [], [] # store all x and y points in list
+class Position:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+ 
+
+def get_bbox_coordinates(handLandmark): 
+    all_x, all_y = [], []
     for hnd in mp_hands.HandLandmark:
-        all_x.append(int(handLandmark.landmark[hnd].x * image_shape[1])) # multiply x by image width
-        all_y.append(int(handLandmark.landmark[hnd].y * image_shape[0])) # multiply y by image height
+        all_x.append(handLandmark.landmark[hnd].x )
+        all_y.append(handLandmark.landmark[hnd].y )
 
-    return min(all_x), min(all_y), max(all_x), max(all_y)
+    position_min = Position(min(all_x), min(all_y))
+    position_max = Position(max(all_x), max(all_y))
+    return position_min, position_max
 
-mp_hands = mp.solutions.mediapipe.python.solutions.hands
-#hands = mp_hands.Hands()
-hands = mp_hands.Hands(
-    static_image_mode=False,
-    model_complexity=1,
-    min_detection_confidence=0.75,
-    min_tracking_confidence=0.75,
-    max_num_hands=2)
-
+mp_hands = mp.solutions.mediapipe.python.solutions.hands 
 mp_draw = mp.solutions.mediapipe.python.solutions.drawing_utils
 
-webcam = cv2.VideoCapture(0)
- 
-w,h = 540,360
-text_color = (0, 0, 0)
-while True: 
-    success, image = webcam.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue 
-    
-    image.flags.writeable = False
-    image_resize = cv2.resize(image, (w, h))
-    image_resize = cv2.flip(image_resize, 1)
-    image_rgb = cv2.cvtColor(image_resize ,cv2.COLOR_BGR2RGB)  
-    results = hands.process(image_rgb)
+hands = mp_hands.Hands()
 
-    #print(results.multi_hand_landmarks)
+file_location = 'videos/03_240p.mp4'
+cap = cv2.VideoCapture(file_location)
+
+cTime, pTime = 0,0
+
+text_color = (255,255,255) 
+
+
+width,height = 540,360
+while True: 
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.") 
+      break 
+
+    #height, width, channels = image.shape 
     
+    image = cv2.resize(image, (width, height)) 
+    image_rgb = cv2.cvtColor(image ,cv2.COLOR_BGR2RGB)  
+    results = hands.process(image_rgb)
+ 
     if results.multi_hand_landmarks:
 
-        if len(results.multi_handedness) == 2 : 
-            text_color = (0,255,0)
-        else :
-            text_color = (0,0,0)
+        # green when 2 hands
+        text_color = (0,255,0) if len(results.multi_handedness) == 2 else (0,0,0) 
         
-        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-            #print('hand_landmarks:', landmark)
-            #print(f'Index finger tip coordinates: (',f'{landmark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * w}, ' f'{landmark.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * h})'  )
-            #mp_draw.draw_landmarks(image_resize, landmark)
-            m = get_bbox_coordinates(hand_landmarks,(h,w))           
-            image_resize = cv2.rectangle(image_resize, (m[0],m[1]), (m[2],m[3]), (255, 0, 0), 1)
-                           
-            index = handedness.classification[0].index
-            label = handedness.classification[0].label
-            if label == 'Left':     
-                cv2.putText(image_resize, label+' Hand('+str(index)+')',(m[0],m[1]), cv2.FONT_HERSHEY_COMPLEX,  0.4, text_color, 1)
+        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness): 
+            mp_draw.draw_landmarks(image, 
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_draw.DrawingSpec(thickness=1,circle_radius=1),
+                mp_draw.DrawingSpec(thickness=1,circle_radius=1))
 
+            pos_min, pos_max = get_bbox_coordinates(hand_landmarks)   
+            cv2.rectangle(image, (int(pos_min.x * width),int(pos_min.y * height)), (int(pos_max.x * width),int(pos_max.y * height)), (255, 0, 0), 1)
+                                   
+            label = handedness.classification[0].label
+            if label == 'Left':      
+                cv2.putText(image, 'Right',  (int(pos_min.x * width),int(pos_min.y * height)), cv2.FONT_HERSHEY_COMPLEX,  0.4, text_color, 1)
+                
             if label == 'Right': 
-                cv2.putText(image_resize, label+' Hand('+str(index)+')', (m[0],m[1]), cv2.FONT_HERSHEY_COMPLEX,  0.4, text_color, 1)
+                cv2.putText(image, 'Left',  (int(pos_min.x * width),int(pos_min.y * height)), cv2.FONT_HERSHEY_COMPLEX,  0.4, text_color, 1)
+               
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(image, str(int(fps)),  (int(0.1 * width),int(0.1 * height)), cv2.FONT_HERSHEY_COMPLEX,  0.4, (255,255,255), 1)
  
-         
-    cv2.imshow("Image",image_resize)
+    cv2.imshow("Image",image)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
